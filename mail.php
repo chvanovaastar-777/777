20cd42eef90d9b720a22a11133d7da2e848b0655
Andrei_CH <chvanovaastar@gmail.com>
2025-10-20 19:51:47 +0500

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,37 @@
+<meta http-equiv='refresh' content='6; url=https://chvanovaastar-777.github.io/777/'>
+
+<meta charset="UTF-8" />
+
+<?php
+
+if (isset($_POST['name'])) {$name = $_POST['name']; if ($name == '') {unset($name);}}
+if (isset($_POST['email'])) {$email = $_POST['email']; if ($email == '') {unset($email);}}
+if (isset($_POST['sub'])) {$sub = $_POST['sub']; if ($sub == '') {unset($sub);}}
+if (isset($_POST['body'])) {$body = $_POST['body']; if ($body == '') {unset($body);}}
+
+ 
+if (isset($name) && isset($email) && isset($sub) && isset($body)){
+
+$address = "mailto:achvanov777@bk.ru";
+
+$mes = "Имя: $name \nE-mail: $email \nТема: $sub \nТекст: $body";
+
+$send = mail ($address,$sub,$mes,"Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
+
+if ($send == 'true')
+
+{echo "Сообщение отправлено успешно, через 6 секунд Вы будете направлены на главную страницу блога <a href='/'>https://chvanovaastar-777.github.io/777/</a>,где сможете продолжить ваш просмотр";}
+
+else {echo "Ошибка, сообщение не отправлено!";}
+
+}
+
+else
+
+{
+21
+echo "Вы заполнили не все поля, вернитесь назад и заполните необходимые поля!";
+
+}
+
+?>
